1ed780d80a0a17c7336f50fea15346f6bb9713ae
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-03-26 16:55:40 -0700

    trying arrow method

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,32 @@
 // 1. Use the D3 library to read the URL
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-
-d3.json(url).then(function(data){
-    console.log(data);
-});
-
+d3.json(url).then((data)=>console.log(data));
 
 // ----------------------------------------------------------------
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    
+// function barChart(){
+//     // Create dropdow menu
+//     let dropdownMenu = d3.select('#selDataset'); 
 
+//     // Assign the data
+//     d3.json(url).then((importedData)=> {
+//         let data=importedData;
+//     });
 
+//     console.log(data)
+    // // Use sample_values as the values for the bar chart.
+    // let sample_values = Object.values(data.samples)
+
+    // // Use otu_ids as the labels for the bar chart.
+    // let otu_ids = Object.keys(data.names)
+
+    // // Use otu_labels as the hovertext for the chart.
+    // let otu_labels = Object.keys(data.names)
+
+    // Plotly.newPlot('bar', data)
+// };
 
 // ----------------------------------------------------------------
 // 3. Create a bubble chart that displays each sample.
