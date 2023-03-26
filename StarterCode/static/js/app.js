4ca51650c4714ae3aaf1bdc70bc7d533c4b518a6
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-03-26 13:05:03 -0700

    used D3 to read in the json url

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,7 @@
+// Use the D3 library to read the URL
+const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
+const data = d3.json(url)
+
+d3.json(url).then(function(data){
+    console.log(data);
+});
