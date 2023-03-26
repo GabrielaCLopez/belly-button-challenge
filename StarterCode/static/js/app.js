f2d2ed1685c4b7b80864d7c9d0b19ca9b8b47790
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-03-26 15:37:19 -0700

    read the URL

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,52 @@
-// Use the D3 library to read the URL
+// 1. Use the D3 library to read the URL
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-const data = d3.json(url)
+
 
 d3.json(url).then(function(data){
     console.log(data);
 });
+
+
+// ----------------------------------------------------------------
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+
+
+// ----------------------------------------------------------------
+// 3. Create a bubble chart that displays each sample.
+
+// Use otu_ids for the x values.
+
+// Use sample_values for the y values.
+
+// Use sample_values for the marker size.
+
+// Use otu_ids for the marker colors.
+
+// Use otu_labels for the text values.
+
+// ----------------------------------------------------------------
+// 4. Display the sample metadata, i.e., an individual's demographic information.
+
+
+// ----------------------------------------------------------------
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+// ----------------------------------------------------------------
+// 6. Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown as follows:
+
+
+// init();
+
+	
+
+
+ 
+
+
+
+
+
+
+
+
