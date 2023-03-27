f7825c40a089195fab5cea68b30c7da6e75d18f8
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-03-26 18:25:24 -0700

    working on setting up charts

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,60 +1,70 @@
 // 1. Use the D3 library to read the URL
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-d3.json(url).then((data)=>console.log(data));
+// d3.json(url).then((data)=>console.log(data));
 
 // ----------------------------------------------------------------
 // Create a horizontal bar chart and a bubble chart, you'll be using the same variables, so create one function that will create both charts
 
 function createCharts(sampleData){
+    const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
-    // // Define sample_values
-    // let sample_values = Object.values(data.samples)
+    // You want to retrieve the data so you can assign variables
+    d3.json(url).then((data)=>{
+        let samples=data.samples
 
-    // // Define otu_ids
-    // let otu_ids = Object.keys(data.names)
+    // Define sample_values
+    let sample_values = Object.values(data.samples)
 
-    // // Define otu_labels as the hovertext
-    // let otu_labels = Object.keys(data.names)
+    // Define otu_ids
+    let otu_ids = Object.keys(data.names)
+
+    // Define otu_labels as the hovertext
+    let otu_labels = Object.keys(data.names)
+
+    // Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    let barTrace = {
+        x: otu_ids,
+        y: sample_values,
+        type: 'bar',
+        orientation: 'h',
+         showlegend: true 
+    };
+
+    let barLayout = {
+        title: "Top 10 OTUs Found in Individual"
+    };
+
+    Plotly.newPlot('bar', barTrace, barLayout)
+
+    // Bubble chart that displays each sample.
+
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    Use otu_labels for the text values.
+        let bubTrace = {
+            x: otu_ids,
+            y: sample_values,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            }
+        };
+
+    let bubLayout = {
+        title: "All Samples"
+    };
+
+    Plotly.newPlot('bubble', bubTrace, bubLayout)
+    });
 }
 
-// 2. Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-//     let barTrace = {
-//         x: otu_ids,
-//         y: sample_values,
-//         type: 'bar',
-//         orientation: 'h',
-//          showlegend: true 
-//     };
 
-//     let barLayout = {
-//         title: "Top 10 OTUs Found in Individual"
-//     };
 
-//     Plotly.newPlot('bar', barTrace, barLayout)
 
-// Bubble chart that displays each sample.
-
-// Use otu_ids for the x values.
-// Use sample_values for the y values.
-// Use sample_values for the marker size.
-// Use otu_ids for the marker colors.
-// Use otu_labels for the text values.
-    // let bubTrace = {
-    //     x: otu_ids,
-    //     y: sample_values,
-    //     mode: 'markers',
-    //     marker: {
-    //         size: sample_values,
-    //         color: otu_ids
-    //     }
-    // };
-
-    // let bubLayout = {
-    //     title: "All Samples"
-    // };
-
-    // Plotly.newPlot('bubble', bubTrace, bubLayout)
 
 // ----------------------------------------------------------------
 // 4. Display the sample metadata, i.e., an individual's demographic information.
